4b8da9262fb4ad72a81637007ea3f30e7686968c
rurabe <rurabe@gmail.com>
2012-10-07 19:41:26 -0700

    fixed SSL

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,7 @@
 class SearchesController < ApplicationController
 
+	before_filter :https_redirect, :only => [:new, :create]
+
 	rescue_from Mecha::AuthenticationError, :with => :error_handling
 
 	def new
@@ -12,7 +14,7 @@ class SearchesController < ApplicationController
 																	:password => params[:search][:password],
 																	:search => @search)
 		if @search.save
-			redirect_to search_url(@search)
+			redirect_to search_url(@search, :protocol => "http")
 		else
 			flash[:error] = @search.errors.full_messages
 			redirect_to new_search_url
@@ -30,6 +32,13 @@ class SearchesController < ApplicationController
 	def error_handling(error)
 		flash[:error] = [error.message]
 		redirect_to new_search_url
+	end
+
+	def https_redirect
+		if !request.ssl?
+	    flash.keep
+	    redirect_to protocol: "https", status: :moved_permanently
+ 		end
 	end
 
 
