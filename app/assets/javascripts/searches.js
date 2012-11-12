ea604f0920a583f1231ca7303cea55c4a2363a94
rurabe <rurabe@gmail.com>
2012-11-11 21:55:19 -0800

    starting again with this

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,5 +1,60 @@
 
 	$(document).ready(function(){
+		// [----------------------------------------===== #NEW =====----------------------------------------]
+		
+		// [----------=====About Tab=====----------]
+
+			$('#about-handle').toggle(
+				function(){
+					var el = $(this);
+					$('.logo-inner').animate({'margin-top': '-350px'});
+					$('.about-info').slideToggle();
+					$(this).fadeOut(200,function(){
+						$(this).text("OK, I got it!").fadeIn();
+					});
+					resizeExplanationDivs();
+				},
+				function(){
+					$('.logo-inner').animate({'margin-top': '-250px'});
+					$('.about-info').slideToggle();
+					$(this).fadeOut(200,function(){
+						$(this).text("Wait, what's this about again?").fadeIn();
+						$('#aboutlink').remove();
+					});
+				}
+			);
+
+		// [----------=====Loading Modal=====----------]
+
+			$('#loadingModal').modal({
+				backdrop: 'static',
+				keyboard: false,
+				show: false
+			});
+
+			$('#login-button').click(function(){
+				$('#icon-carousel').carousel({
+					interval: 2500,
+					pause: "false"
+				}).carousel('cycle');
+				$('#loadingModal').modal('show');
+			})
+
+		// [----------=====Keep the divs the same size =====----------]
+	 resizeExplanationDivs = function(){
+	 		$('p.about-explanation').map(function(){$(this).removeAttr("style")});
+			var sizes = $('p.about-explanation').map(function(){ return $(this).height() }).get();
+			var maxSize = Math.max.apply(null,sizes);
+			$('p.about-explanation').map(function(){$(this).height(maxSize)});
+		}
+		
+		$(window).resize(function(){ resizeExplanationDivs(); })
+
+
+	});
+
+
+	$(document).ready(function(){
 		// [----------------------------------------===== #SHOW =====----------------------------------------]
 
 			// Format bookstore sold outs as labels on load
@@ -153,7 +208,7 @@
 						}
 					})
 					priceDivsHeartShapedBox.divs.push(thisDiv);
-					thisDiv.makeExpress();
+					thisDiv.toExpressDiv();
 					return thisDiv;
 				};
 
@@ -179,7 +234,7 @@
 
 					})
 					priceDivsHeartShapedBox.divs.push(thisDiv);
-					thisDiv.makeExpress();
+					thisDiv.toExpressDiv();
 					return thisDiv;
 				};
 
@@ -351,3 +406,4 @@
 
 			
 	});
+
