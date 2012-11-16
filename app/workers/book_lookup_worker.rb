0690a51fb9e95cb545f6249705f92fcc44f85630
rurabe <rurabe@gmail.com>
2012-11-16 14:14:10 -0800

    trying to debug

--- app/workers/book_lookup_worker.rb
+++ app/workers/book_lookup_worker.rb
@@ -4,6 +4,7 @@ class BookLookupWorker
 	def perform(vendor,search_id)
 		search = Search.find(search_id)
 		data = search.lookup(vendor).ui_data.to_json
-		PrivatePub.publish_to("/" + search.slug,"BOOKSUPPLYCO.importData(#{data})")
+		p "working"
+		PrivatePub.publish_to("/" + search.slug,"BOOKSUPPLYCO.importData(#{data});console.log('received #{vendor}');")
 	end
 end
--- app/workers/used_bn_book_lookup_worker.rb
+++ app/workers/used_bn_book_lookup_worker.rb
@@ -4,6 +4,7 @@ class UsedBnBookLookupWorker
 	def perform(ean,search_id)
 		search = Search.find(search_id)
 		data = BarnesAndNoble::UsedBooks.new(ean).ui_data.to_json
-		PrivatePub.publish_to("/" + search.slug,"BOOKSUPPLYCO.importData(#{data})")
+		p "working"
+		PrivatePub.publish_to("/" + search.slug,"BOOKSUPPLYCO.importData(#{data});console.log('received #{ean}');")
 	end
 end
