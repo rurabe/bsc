f2b261a07711efc4e706109710386208cab515df
rurabe <rurabe@gmail.com>
2012-12-21 04:12:51 -1000

    js wasnt compiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ BookSupply::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( *booklists* )
+  config.assets.precompile += %w( *booklists* *schools* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
