ccea5c5e9715d8b77a2fbc52b0777e01978431bf
rurabe <rurabe@gmail.com>
2013-01-25 02:27:33 -0800

    tweaked saddleback

--- app/concerns/mecha/saddleback.rb
+++ app/concerns/mecha/saddleback.rb
@@ -84,13 +84,9 @@ module Mecha
 
       # Book_data helpers #
       def get_book_nodes(node)
-        page = query_for_book(node)
-        page.search(".//ul[preceding-sibling::comment()[contains(concat(' ',.,' '),'start the bookResultsInfo')]]")
-      end
-
-      def query_for_book(node)
         junk_mecha = Mechanize.new { |mecha| mecha.follow_meta_refresh = true }
         page = junk_mecha.post('http://www.bkstr.com/webapp/wcs/stores/servlet/booklookServlet',build_book_params(node))
+        page.search(".//ul[preceding-sibling::comment()[contains(concat(' ',.,' '),'start the bookResultsInfo')]]")
       end
         
       def build_book_params(node)
