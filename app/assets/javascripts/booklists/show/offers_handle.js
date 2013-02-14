03d6f4a592567a71e7f9f7f5b82a6b2594214614
Ryan Urabe <rurabe@gmailcom>
2013-02-14 01:07:01 -0800

    merging with v2

--- app/assets/javascripts/booklists/show/offers_handle.js
+++ app/assets/javascripts/booklists/show/offers_handle.js
@@ -1,42 +1,48 @@
 var createOffersHandle = function(offerGroup){
   var $el = offerGroup.$el.find('.book-button-handle');
-  var wrapper = $el.find('.book-button-handle-inner')
+  var wrapper = $el.find('.handle-content');
+
+  var moreOptions = '<span class="handle-content">more options</span>'
   var arrows = '<i class="icon-double-angle-down"></i>'
-
-  var changeContent = function(content,callbackOut,callbackIn){
-    callbackIn = callbackIn || $.noop;
-    callbackOut = callbackOut || $.noop;
-    wrapper.fadeOut(50,function(){
-      callbackIn.call();
-      wrapper.html(content).fadeIn(50,function(){
-        callbackOut.call();
-      });
-    });
-  };
-
   var setLabelChanger = function(){
-    $el.mouseenter(function(){
-      changeContent("more options",function(){
-        $el.mouseleave(function(){
-          changeContent(arrows);
-          $el.animate({'width': '50%',
+    $el.mouseenter(function(e){
+      e.stopPropagation();
+      changeContent(moreOptions);
+      $(this).animate({'width': '70%',
+                       'color': 'white',
+                       'background-color': 'black'},50);
+    }).mouseleave(function(e){
+      e.stopPropagation();
+      changeContent(arrows);
+      $(this).animate({'width': '50%',
                        'color': 'black',
-                       'background-color': 'white'},100);
-        });
-      });
-      $el.animate({'width': '70%',
-                   'color': 'white',
-                   'background-color': 'black'},100);
+                       'background-color': 'white'},50);
     });
   };
 
-
+  var $bookRow = offerGroup.book.$el
+  var offerRow = '<div class="span5 offer-row"><%= offer.formattedPrice %></div>'
+  var setClickOpener = function(){
+    $el.click(function(){
+      _.each(offerGroup.offers,function(offer){
+        $bookRow.append(_.template(offerRow,{offer: offer}));
+      });
+    })
+  }
 
   returnObject = {
-    $el: $el
+    $el: $el,
+    offerGroup: offerGroup
+  };
+  var changeContent = function(content){
+    wrapper.fadeOut(50,function(){
+      wrapper.html(content).fadeIn(50);
+    });
   };
 
   setLabelChanger();
+  setClickOpener();
+
 
   return returnObject;
 };
